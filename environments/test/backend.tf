61387afe45485e4e4305333306348149daa70381
v-pham04 <phamb0532@my.uwstout.edu>
2026-06-12 17:37:04 -0500

    Formatted the three files flagged by the pipeline and verified the check passes locally

--- environments/test/backend.tf
+++ environments/test/backend.tf
@@ -1,6 +1,6 @@
 terraform {
-    backend "s3" {
+  backend "s3" {
     # Configuration is injected dynamically during local initialization 
     # to protect sensitive cloud infrastructure architecture footprints.    
-    }
+  }
 }
